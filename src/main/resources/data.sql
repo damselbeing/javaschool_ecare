51a4ae64cfbe43e12f2a206d30f8b922dc235482
damselbeing <damselbeing@gmail.com>
2022-02-20 18:25:42 +0300

    data.sql file was added together with the related properties.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO clientdb
+    (id, name, last_name, birth_date, passport, address, e_mail, password) VALUES
+                                           (1, 'mike', 'autumn', '2003-03-03', '3333 333333', 'kazan', 'mike@mail', 'fghijk');
