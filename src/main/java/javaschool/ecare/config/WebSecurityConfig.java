1e25752044b78e5026b6cf3cf6513ba7ebdf3260
damselbeing <damselbeing@gmail.com>
2022-04-12 14:47:47 +0300

    httpSecurity.csrf().disable() was added.

--- src/main/java/javaschool/ecare/config/WebSecurityConfig.java
+++ src/main/java/javaschool/ecare/config/WebSecurityConfig.java
@@ -23,8 +23,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf()
-                .disable()
+//                .csrf()
+//                .disable()
                 .authorizeRequests()
 
                     //Доступ только для не зарегистрированных пользователей
@@ -55,6 +55,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                     .permitAll()
                     .logoutSuccessUrl("/login");
+
+        httpSecurity.csrf().disable();
     }
 
     @Autowired
